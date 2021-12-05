6dd0f7c349932f8ec5770c410faa74ed3e6c8445
eomar <25548321+Eomar21@users.noreply.github.com>
2021-12-05 18:56:09 +0100

    Added NavBar and basic routing for first two pages

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import Home from './components/Home';
+import AboutUs from './components/AboutUs';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import NavBar from './components/NavBar';
+
 
 function App() {
   return (
-    <div className="App">
-      <header>
-      </header>
-    </div>
+    <Router>
+      <NavBar/>
+      <Routes>
+        <Route exact path="/" element={<Home/>}></Route>
+        <Route exact path="/AboutUs" element={<AboutUs/>}></Route>
+    </Routes>
+    </Router >
   );
 }
 
