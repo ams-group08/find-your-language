c3d68e1129081fc9ec6cee7826fb28e04ef17fcb
eomar21 <25548321+Eomar21@users.noreply.github.com>
2021-12-06 12:51:57 +0100

    Added few missing layout elements

--- webapp/src/components/LineChart.js
+++ webapp/src/components/LineChart.js
@@ -3,41 +3,43 @@ import { Chart } from "react-google-charts";
 
 
 export class LineChart extends Component {
-    render() {
-        return (
-            <div>
-                <Chart
-  width={Window.innerWidth}
-  height={'400px'}
-  chartType="LineChart"
-  loader={<div>Loading Chart</div>}
-  data={[
-    ['x', 'dogs', 'cats'],
-    [0, 0, 0],
-    [1, 10, 5],
-    [2, 23, 15],
-    [3, 17, 9],
-    [4, 18, 10],
-    [5, 9, 5],
-    [6, 11, 3],
-    [7, 27, 19],
-  ]}
-  options={{
-    hAxis: {
-      title: 'Time',
-    },
-    vAxis: {
-      title: 'Popularity',
-    },
-    series: {
-      1: { curveType: 'function' },
-    },
-  }}
-  rootProps={{ 'data-testid': '2' }}
-/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Chart
+          width={Window.innerWidth}
+          height={'400px'}
+          chartType="LineChart"
+          loader={<div>Loading Chart</div>}
+          data={[
+            ['x', "C#", "Python", "C", "Ruby"],
+            [0, 0, 0, 0, 0],
+            [1, 10, 5, 6, 0],
+            [2, 23, 15, 22, 1],
+            [3, 17, 9, 49, 2],
+            [4, 18, 10, 15, 4],
+            [5, 9, 5, 55, 30],
+            [6, 11, 3, 29, 25],
+            [7, 27, 19, 4, 20],
+          ]}
+          options={{
+            hAxis: {
+              title: 'Time',
+            },
+            vAxis: {
+              title: 'Popularity',
+            },
+            series: {
+              1: { curveType: 'function' },
+              2: { curveType: 'function' },
+              3: { curveType: 'function' },
+            },
+          }}
+          rootProps={{ 'data-testid': '2' }}
+        />
+      </div>
+    )
+  }
 }
 
 export default LineChart
--- webapp/src/components/NavBar.js
+++ webapp/src/components/NavBar.js
@@ -6,7 +6,7 @@ export class NavBar extends Component {
         return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
                 <div className="container-fluid">
-                    <Link  className="navbar-brand" to="/">Language Popularity</Link>
+                    <Link  className="navbar-brand text-info display-4" to="/">Language Popularity</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
