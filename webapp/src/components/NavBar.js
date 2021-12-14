cf6cfad89b899356e599ad4dafa785cf0de25cb2
eomar <25548321+Eomar21@users.noreply.github.com>
2021-12-14 20:22:36 +0100

    disabled trends for now

--- webapp/src/components/NavBar.js
+++ webapp/src/components/NavBar.js
@@ -20,9 +20,9 @@ export class NavBar extends Component {
                                 <li className="nav-item">
                                     <Link className="nav-link" aria-current="page" to="/">Home</Link>
                                 </li>
-                                <li className="nav-item">
+                                {/* <li className="nav-item">
                                     <Link className="nav-link" to="/Trends">Trends</Link>
-                                </li>
+                                </li> */}
                                 <li className="nav-item">
                                     <Link className="nav-link" to="/AboutUs">About Us</Link>
                                 </li>
