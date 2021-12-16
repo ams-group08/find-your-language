c1310c39e41e40c6bfd5b52818e24b6974856449
eomar21 <25548321+Eomar21@users.noreply.github.com>
2021-12-16 17:19:24 +0100

    added google trend to the backend

--- backend/main.js
+++ backend/main.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const googleTrends = require('google-trends-api');
 
-app.use(express.static(path.join(__dirname,'build')))
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname,'build', 'index.html'))
+app.use(express.static(path.join(__dirname, '../webapp/build')))
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../webapp/build', 'index.html'))
 });
 
-app.listen(3001);
+app.get('/users', function (req, res) {
+    console.log("here you go");
+    res.send('here you go')
+})
+
+app.get('/googletrends', function(req,res){
+    getTrendResult();
+    res.send('done')
+})
+
+function getTrendResult() {
+    googleTrends.interestOverTime({ keyword: ['C#', 'javascript'] })
+        .then(function (results) {
+            console.log('These results are awesome', results);
+        })
+        .catch(function (err) {
+            console.error('Oh no there was an error', err);
+        });
+}
+
+
+app.listen(3003);
