ad90395865c31b1891ccff6de878298874996168
eomar <25548321+Eomar21@users.noreply.github.com>
2021-12-16 17:49:05 +0100

    google trends searching from the backend

--- backend/main.js
+++ backend/main.js
@@ -13,20 +13,16 @@ app.get('/users', function (req, res) {
     res.send('here you go')
 })
 
-app.get('/googletrends', function(req,res){
-    getTrendResult();
-    res.send('done')
+app.get('/googletrends', function (req,res) {
+    googleTrends.interestOverTime({keyword: req.query.keyword })
+    .then(function(results){
+      res.status(200).json(results);
+    })
+    .catch(function(err){
+      console.error('Oh no there was an error', err);
+    });
 })
 
-function getTrendResult() {
-    googleTrends.interestOverTime({ keyword: ['C#', 'javascript'] })
-        .then(function (results) {
-            console.log('These results are awesome', results);
-        })
-        .catch(function (err) {
-            console.error('Oh no there was an error', err);
-        });
-}
 
 
 app.listen(3003);
