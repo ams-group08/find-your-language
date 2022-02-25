791f7d98404289610c6f39c51d0b7a0f7a6ce3e0
eomar <25548321+Eomar21@users.noreply.github.com>
2022-02-25 17:07:46 +0100

    updated text for all graph pages

--- alternative/findyourasp/find-your-language/ClientApp/src/components/GraphGithub.js
+++ alternative/findyourasp/find-your-language/ClientApp/src/components/GraphGithub.js
@@ -6,12 +6,12 @@ import GraphTitle from './GraphTitle';
 export class GraphGithub extends Component {
     scrollToTop() {
         window.scrollTo({
-          top: 0,
-          behavior: "smooth"
+            top: 0,
+            behavior: "smooth"
         });
-      }
-    componentDidMount(){
-      this.scrollToTop()
+    }
+    componentDidMount() {
+        this.scrollToTop()
     }
     render() {
         return <div>
@@ -19,8 +19,14 @@ export class GraphGithub extends Component {
                 <div className="col-10">
                     <GraphTitle sourcename="Github" ></GraphTitle>
                     <br></br>
-                    <p className='text' >GitHub, Inc. is a provider of Internet hosting for software development and version control using Git. It offers the distributed version control and source code management functionality of Git, plus its own features.</p>
-                    <p className='text'>Professional and hobby developers use Github as a repository for their code. By looking at how the languages of interest are used in public projects it should be possible to provide a measure of how popular these languages are in real-world applications.</p>
+                    <p className='text' >GitHub, Inc. is a provider of Internet hosting for software development and version control using Git. It offers the distributed version control and source code management functionality of Git, plus its own features.
+                        Professional and hobby developers use Github as a repository for their code. By looking at how the languages of interest are used in public projects it should be possible to provide a measure of how popular these languages are in real-world applications.</p>
+                    <p className='text'>Each repository has a coding language associated with it - this is the predominant language used in the files that are managed in GitHub. GitHub generates this information. This makes GitHub an ideal source of per-language metrics. There are several options for metrics to examine. Each repository has the following feature: stars, watchers, forks - described below. Any one of these can be used to interpret how often a repository is being used/implemented/looked at.</p>
+                    <ul>
+                        <li className='text'>Stars: Starring makes it easy to find a repository or topic again later. Generally a developer will star repositories and topics to keep track of projects they find interesting.Many of GitHub's repository rankings depend on the number of stars a repository has.</li>
+                        <li className='text'>Watch: When a developer chooses to watch a project, they will be notified whenever there are any updates. This is different from the "star" system which does not provide notifications. Again, this is a good metric of how often other users are taking an interest or using a repo frequently enough that they require updates on any changes.</li>
+                        <li className='text'>Fork: A fork is a copy of a repository. Forking a repository allows a developer to freely experiment with changes without affecting the original project. Again, this is an excellent indication of how frequently the code base and thus language is being actively used by developers.</li>
+                    </ul>
                     <SeeOurDocument link="https://github.com/ams-group08/find-your-language/blob/main/documentation/GITHUB.md"></SeeOurDocument>
                     <br></br>
                     <LineChart call="trends/github" source="Github" vaxistitle="Percentage" ></LineChart>
--- alternative/findyourasp/find-your-language/ClientApp/src/components/GraphGoogleTrend.js
+++ alternative/findyourasp/find-your-language/ClientApp/src/components/GraphGoogleTrend.js
@@ -5,12 +5,12 @@ import GraphTitle from './GraphTitle';
 export class GraphGoogleTrend extends Component {
     scrollToTop() {
         window.scrollTo({
-          top: 0,
-          behavior: "smooth"
+            top: 0,
+            behavior: "smooth"
         });
-      }
-    componentDidMount(){
-      this.scrollToTop()
+    }
+    componentDidMount() {
+        this.scrollToTop()
     }
     render() {
         return <div>
@@ -18,7 +18,11 @@ export class GraphGoogleTrend extends Component {
                 <div className="col-10">
                     <GraphTitle sourcename="Google Trends" ></GraphTitle>
                     <br></br>
-                    <p className='text' >We used the data provided by google trends which represents the search trends along the past years over the search engine, then we we came to this conclusion.</p>
+                    <p className='text' >Google launched Google Insights for Search, a more sophisticated and advanced service displaying search trends data. On September 27, 2012, Google merged Google Insights for Search into Google Trends.
+                        Google Trends is a useful search trend feature that shows how frequently a given search term is entered into Google’s search engine relative to the site’s total search volume over a given period of time. So, in other words, it analyzes the popularity of top search queries across various regions and languages.  The website uses graphs to compare the search volume of different queries over time.</p>
+                    <p className='text' >Google Trends can be used for comparative keyword research and to discover event-triggered spikes in keyword search volume.
+                        You can explore Google Trends from the top down by visiting the tool and seeing what searches are currently trending, then digging into a topic for more information.
+                        You can also enter a search term into the search box at the top of the tool to see how search volume has varied for that term over time and in different locations. You change the location, time frame, category or industry, and type of search (web, news, shopping, or YouTube) for more fine-grained data.</p>
                     <br></br>
                     <LineChart call="trends/googledata" source="Google Trends" vaxistitle="Popularity"></LineChart>
                     <br></br>
